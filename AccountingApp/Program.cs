df8ae331a02dd7234a71f8a9c3f2ff242929ce6d
saeedmaghdam <smasafat@gmail.com>
2024-07-12 14:49:09 +0200

    add gui-cs to demo app

--- AccountingApp/Program.cs
+++ AccountingApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Terminal.Gui;
+
+Application.Init();
+
+Application.Run(new AccountingApp.MainForm.MainForm());
+
+Application.Shutdown();
